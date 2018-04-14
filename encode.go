3c13aa3d1d042252b623b09cb16e31852c724b76
Yutaka Ichibangase <yichiban@gmail.com>
2018-04-14 20:14:24 +0900

    reduce gocyclo: encode

--- encode.go
+++ encode.go
@@ -42,78 +42,97 @@ func (e *Encoder) Encode(val interface{}) error {
 		v = v.Elem()
 		return e.Encode(v.Interface())
 	case reflect.Map:
-		var buf bytes.Buffer
-		f := Encoder{
-			Writer: &buf,
-		}
-		ks := v.MapKeys()
-		sort.Slice(ks, func(i, j int) bool {
-			return ks[i].String() < ks[j].String()
-		})
-		for _, k := range ks {
-			if err := f.Encode(k.Interface()); err != nil {
-				return err
-			}
-			if err := f.Encode(v.MapIndex(k).Interface()); err != nil {
-				return err
-			}
-		}
-		_, err := fmt.Fprintf(e, "%d:%s}", buf.Len(), buf.Bytes())
-		return err
+		return e.encodeMap(v)
 	case reflect.Struct:
-		var buf bytes.Buffer
-		f := Encoder{
-			Writer: &buf,
-		}
-		for i := 0; i < v.NumField(); i++ {
-			t := v.Type().Field(i)
-			fv := v.Field(i)
-
-			if !fv.CanInterface() {
-				continue
-			}
-
-			name := t.Name
-			if tag, ok := t.Tag.Lookup("tnetstrings"); ok {
-				if tag == "-" {
-					continue
-				}
-				ts := strings.Split(tag, ",")
-				if len(ts) > 0 && ts[0] != "" {
-					name = ts[0]
-				}
-				if len(ts) > 1 && ts[1] == "omitempty" && fv == reflect.Zero(t.Type) {
-					continue
-				}
-			}
-
-			if err := f.Encode(name); err != nil {
-				return err
-			}
-			if err := f.Encode(fv.Interface()); err != nil {
-				return err
-			}
-		}
-		_, err := fmt.Fprintf(e, "%d:%s}", buf.Len(), buf.Bytes())
-		return err
+		return e.encodeStruct(v)
 	case reflect.Array, reflect.Slice:
-		if v.Type().Elem().Kind() == reflect.Uint8 {
-			s := fmt.Sprintf("%s", val)
-			_, err := fmt.Fprintf(e, "%d:%s,", len(s), s)
+		return e.encodeSlice(v)
+	}
+	return ErrUnsupportedType{Type: v.Type()}
+}
+
+func (e *Encoder) encodeMap(v reflect.Value) error {
+	var buf bytes.Buffer
+	f := Encoder{
+		Writer: &buf,
+	}
+	ks := v.MapKeys()
+	sort.Slice(ks, func(i, j int) bool {
+		return ks[i].String() < ks[j].String()
+	})
+	for _, k := range ks {
+		if err := f.Encode(k.Interface()); err != nil {
 			return err
 		}
-		var buf bytes.Buffer
-		f := Encoder{
-			Writer: &buf,
+		if err := f.Encode(v.MapIndex(k).Interface()); err != nil {
+			return err
 		}
-		for i := 0; i < v.Len(); i++ {
-			if err := f.Encode(v.Index(i).Interface()); err != nil {
-				return err
-			}
-		}
-		_, err := fmt.Fprintf(e, "%d:%s]", buf.Len(), buf.Bytes())
-		return err
-	default:
-		return ErrUnsupportedType{Type: v.Type()}
 	}
+	_, err := fmt.Fprintf(e, "%d:%s}", buf.Len(), buf.Bytes())
+	return err
+}
+
+func (e *Encoder) encodeStruct(v reflect.Value) error {
+	var buf bytes.Buffer
+	f := Encoder{
+		Writer: &buf,
+	}
+	for i := 0; i < v.NumField(); i++ {
+		name, val, ok := field(v, i)
+		if !ok {
+			continue
+		}
+
+		if err := f.Encode(name); err != nil {
+			return err
+		}
+		if err := f.Encode(val); err != nil {
+			return err
+		}
+	}
+	_, err := fmt.Fprintf(e, "%d:%s}", buf.Len(), buf.Bytes())
+	return err
+}
+
+func field(v reflect.Value, i int) (string, interface{}, bool) {
+	t := v.Type().Field(i)
+	fv := v.Field(i)
+
+	if !fv.CanInterface() {
+		return "", nil, false
+	}
+
+	name := t.Name
+	if tag, ok := t.Tag.Lookup("tnetstrings"); ok {
+		if tag == "-" {
+			return "", nil, false
+		}
+		ts := strings.Split(tag, ",")
+		if len(ts) > 0 && ts[0] != "" {
+			name = ts[0]
+		}
+		if len(ts) > 1 && ts[1] == "omitempty" && fv == reflect.Zero(t.Type) {
+			return "", nil, false
+		}
+	}
+	return name, fv.Interface(), true
+}
+
+func (e *Encoder) encodeSlice(v reflect.Value) error {
+	if v.Type().Elem().Kind() == reflect.Uint8 {
+		s := fmt.Sprintf("%s", v.Interface())
+		_, err := fmt.Fprintf(e, "%d:%s,", len(s), s)
+		return err
+	}
+	var buf bytes.Buffer
+	f := Encoder{
+		Writer: &buf,
+	}
+	for i := 0; i < v.Len(); i++ {
+		if err := f.Encode(v.Index(i).Interface()); err != nil {
+			return err
+		}
+	}
+	_, err := fmt.Fprintf(e, "%d:%s]", buf.Len(), buf.Bytes())
+	return err
 }
